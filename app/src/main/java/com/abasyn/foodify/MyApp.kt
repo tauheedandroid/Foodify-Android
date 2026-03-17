be5d1d2477c13887337efa70970797fdc72f1fac
Your Name <you@example.com>
2026-03-17 18:48:29 +0500

    Initial Commit

--- app/src/main/java/com/abasyn/foodify/MyApp.kt
+++ app/src/main/java/com/abasyn/foodify/MyApp.kt
@@ -2,10 +2,8 @@ package com.abasyn.foodify
 
 import android.app.Application
 import com.abasyn.foodify.di.appModule
-import com.google.android.material.shape.ShapeAppearanceModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
-import org.koin.core.module.Module
 
 class MyApp : Application() {
     override fun onCreate() {
